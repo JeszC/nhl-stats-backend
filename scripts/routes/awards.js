1efb0666085f2e0d2aeef322567ae842dc148f5f
Jamppah <@>
2025-06-24 23:14:00 +0300

    Clean up horrendous URL

--- scripts/routes/awards.js
+++ scripts/routes/awards.js
@@ -1,11 +1,16 @@
 import {getResponseData} from "../shared/utils.js";
 
+const baseURL = "https://records.nhl.com/site/api/award-details?cayenneExp=";
+const playerFields = "&include=player.firstName&include=player.lastName&include=player.position";
+const teamFields = "&include=team.placeName&include=team.commonName&include=team.triCode&include=team.logos";
+
 export async function getTrophies() {
     let response = await fetch("https://records.nhl.com/site/api/trophy");
     return await getResponseData(response, "data");
 }
 
 export async function getTrophyWinners(trophyCategoryID, trophyID) {
-    let response = await fetch(`https://records.nhl.com/site/api/award-details?cayenneExp=trophyCategoryId=${trophyCategoryID}%20and%20trophyId=${trophyID}&include=player.firstName&include=player.lastName&include=player.position&include=team.fullName&include=team.placeName&include=team.commonName&include=team.triCode&include=team.logos`);
+    let url = `${baseURL}trophyCategoryId=${trophyCategoryID} and trophyId=${trophyID}${playerFields}${teamFields}`;
+    let response = await fetch(url);
     return await getResponseData(response, "data");
 }
