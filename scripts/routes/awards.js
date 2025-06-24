a3691b28750ea2150b1790dc714cc117935b4cc8
Jamppah <@>
2025-06-24 14:34:05 +0300

    Fix URL not working due to template string whitespace on linebreak

--- scripts/routes/awards.js
+++ scripts/routes/awards.js
@@ -6,9 +6,6 @@ export async function getTrophies() {
 }
 
 export async function getTrophyWinners(trophyCategoryID, trophyID) {
-    let response = await fetch(`https://records.nhl.com/site/api/award-details
-    ?cayenneExp=trophyCategoryId=${trophyCategoryID}%20and%20trophyId=${trophyID}
-    &include=player.firstName&include=player.lastName&include=player.position&include=team.fullName
-    &include=team.placeName&include=team.commonName&include=team.triCode&include=team.logos`);
+    let response = await fetch(`https://records.nhl.com/site/api/award-details?cayenneExp=trophyCategoryId=${trophyCategoryID}%20and%20trophyId=${trophyID}&include=player.firstName&include=player.lastName&include=player.position&include=team.fullName&include=team.placeName&include=team.commonName&include=team.triCode&include=team.logos`);
     return await getResponseData(response, "data");
 }
